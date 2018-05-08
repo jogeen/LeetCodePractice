893c2664a2c2aaf6ecfee8359b401f2840759943
jogeen <18200390319@163.com>
2018-05-08 17:52:48 +0800

    solution1

---
+++ src/easy/solution1/Solution.java
@@ -0,0 +1,24 @@
+package easy.solution1;
+
+public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		for (int i = 0; i < nums.length-1; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+				if(nums[i]+nums[j]==target) {
+					result[0]=i;
+					result[1]=j;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 12, 7, 11, 15,2 };
+		int target = 9;
+		int[] twoSum = new Solution().twoSum(nums, target);
+		System.out.println(twoSum[0]+"+"+twoSum[1]);
+	}
+}
