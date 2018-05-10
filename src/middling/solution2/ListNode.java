72ce8a40a40bb0b8059bc542fce792b982c3f237
jogeen <18200390319@163.com>
2018-05-10 09:12:50 +0800

    solution2-3

---
+++ src/middling/solution2/ListNode.java
@@ -0,0 +1,10 @@
+package middling.solution2;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
---
+++ src/middling/solution3/Solution.java
@@ -0,0 +1,36 @@
+package middling.solution3;
+
+public class Solution {
+
+	public int lengthOfLongestSubstring(String s) {
+		int maxLength = 0;
+		java.util.Map<Character, Integer> map = new java.util.HashMap<>();
+		Integer startIndex = 0;
+		Integer preIndex = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				preIndex = map.get(c);
+				Integer length = i - preIndex;
+				if (length > maxLength) {
+					maxLength = length;
+				}
+				for (int j = startIndex; j <= preIndex; j++) {
+					Integer index = map.get(s.charAt(j));
+					if(index!=null&&index<=preIndex) {
+						map.remove(s.charAt(j));
+					}
+				}
+				startIndex = new Integer(preIndex);
+			}
+			map.put(c, i);
+			maxLength = map.size() > maxLength ? map.size() : maxLength;
+		}
+		return maxLength;
+	}
+
+	public static void main(String[] args) {
+		int lengthOfLongestSubstring = new Solution().lengthOfLongestSubstring("abcabcbb");
+		System.out.println(lengthOfLongestSubstring);
+	}
+}
